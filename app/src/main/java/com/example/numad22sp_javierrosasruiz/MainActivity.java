806246324bc326d54f44d012aa8e5282838479bc
javier-rosas <jr2dj@virginia.edu>
2022-01-21 09:52:09 -0500

    Initial commit

---
+++ app/src/main/java/com/example/numad22sp_javierrosasruiz/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.numad22sp_javierrosasruiz;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    private static final String TAG = "MainActivity";
+    Button button1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button1 = findViewById(R.id.button);
+
+        button1.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Toast.makeText(MainActivity.this, "             Javier \n rosasruiz.j@northeastern.edu", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
