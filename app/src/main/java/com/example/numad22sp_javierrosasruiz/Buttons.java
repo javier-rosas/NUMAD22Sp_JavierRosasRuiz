f32a8095941368ee79b23668a5cb1b3941e1eee4
javier-rosas <jr2dj@virginia.edu>
2022-02-02 21:47:17 -0500

    Added files to neu repository

---
+++ app/src/main/java/com/example/numad22sp_javierrosasruiz/Buttons.java
@@ -0,0 +1,117 @@
+package com.example.numad22sp_javierrosasruiz;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Buttons extends AppCompatActivity implements View.OnClickListener
+{
+    private static final String TAG = "Buttons";
+
+    Button a;
+    Button b;
+    Button c;
+    Button d;
+    Button e;
+    Button f;
+
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.buttonscreen);
+
+        a = findViewById(R.id.buttonA);
+        b = findViewById(R.id.buttonB);
+        c = findViewById(R.id.buttonC);
+        d = findViewById(R.id.buttonD);
+        e = findViewById(R.id.buttonE);
+        f = findViewById(R.id.buttonF);
+
+        a.setOnClickListener(Buttons.this);
+        b.setOnClickListener(Buttons.this);
+        c.setOnClickListener(Buttons.this);
+        d.setOnClickListener(Buttons.this);
+        e.setOnClickListener(Buttons.this);
+        f.setOnClickListener(Buttons.this);
+
+
+        /*
+        a.setOnClickListener(new View.OnClickListener()
+        {
+
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+         */
+
+
+    }
+
+    @Override
+    public void onClick(View view)
+    {
+        TextView pressedText = findViewById(R.id.pressed);
+
+
+        switch (view.getId()) {
+            case R.id.buttonA:
+
+                Button buttonA = findViewById(R.id.buttonA);
+                String buttonTextA = buttonA.getText().toString();
+                pressedText.setText("Pressed: " + buttonTextA);
+
+                break;
+            case R.id.buttonB:
+
+                Button buttonB = findViewById(R.id.buttonB);
+                String buttonTextB = buttonB.getText().toString();
+                pressedText.setText("Pressed: " + buttonTextB);
+
+                break;
+            case R.id.buttonC:
+
+                Button buttonC = findViewById(R.id.buttonC);
+                String buttonTextC = buttonC.getText().toString();
+                pressedText.setText("Pressed: " + buttonTextC);
+
+                break;
+            case R.id.buttonD:
+
+                Button buttonD = findViewById(R.id.buttonD);
+                String buttonTextD = buttonD.getText().toString();
+                pressedText.setText("Pressed: " + buttonTextD);
+
+                break;
+
+            case R.id.buttonE:
+
+                Button buttonE = findViewById(R.id.buttonE);
+                String buttonTextE = buttonE.getText().toString();
+                pressedText.setText("Pressed: " + buttonTextE);
+
+                break;
+            case R.id.buttonF:
+                Button buttonF = findViewById(R.id.buttonF);
+                String buttonTextF = buttonF.getText().toString();
+                pressedText.setText("Pressed: " + buttonTextF);
+                break;
+
+        }
+
+
+    }
+
+
+
+
+}
--- app/src/main/java/com/example/numad22sp_javierrosasruiz/MainActivity.java
+++ app/src/main/java/com/example/numad22sp_javierrosasruiz/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.numad22sp_javierrosasruiz;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -13,6 +15,8 @@ public class MainActivity extends AppCompatActivity
     private static final String TAG = "MainActivity";
     Button button1;
 
+    Button button2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -20,15 +24,26 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
 
         button1 = findViewById(R.id.button);
+        button2 = findViewById(R.id.button2);
 
         button1.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View view)
             {
-                System.out.print("It Worked!");
-
+                Log.d(TAG, "onClick: Button 1 pressed!");
                 Toast.makeText(MainActivity.this, "             Javier \n rosasruiz.j@northeastern.edu", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Log.d(TAG, "onClick: Button 2 pressed!");
+                startActivity(new Intent(MainActivity.this, Buttons.class));
+
             }
         });
     }
